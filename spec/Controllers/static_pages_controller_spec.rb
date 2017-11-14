48c4bd8533c37b1c81555b44a03711ea74e274c0
Sistla <SS055088@cerner.net>
2017-11-14 11:46:59 -0600

    Created static pages with test specs

---
+++ spec/Controllers/static_pages_controller_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe StaticPagesController, type: :controller do
+
+  describe '#home' do
+    context 'when home action is requested' do
+      subject { get :home }
+
+      it 'gets home' do
+        expect(subject).to have_http_status(:success)
+      end
+    end
+  end
+
+  describe '#help' do
+    context 'when help action is requested' do
+      subject { get :help }
+
+      it 'gets help' do
+        expect(subject).to have_http_status(:success)
+      end
+    end
+  end
+
+  describe '#about' do
+    context 'when about action is requested' do
+      subject { get :about }
+
+      it 'gets about' do
+        expect(subject).to have_http_status(:success)
+      end
+    end
+  end
+end
